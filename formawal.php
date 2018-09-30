27c7712e19bb7f78aaa5e7689fd156903e30df18
achileusc <43493889+achileusc@users.noreply.github.com>
2018-09-30 14:24:25 +0700

    Update formawal.php
    Achileus Valentino Carada
    6701170079
    D3MI-41-04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,28 @@
+<form action="" method="post">
+	<table>
+		<tr>
+			<td>Username</td>
+			<td>:</td>
+			<td><input type="text" name="user"></td><br>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td>:</td>
+			<td><input type="password" name="pass"></td><br>
+		</tr>
+		<tr>
+			<td><input type="submit" name="submit" value="Kirim"></td>
+		</tr>
+	</table>
+</form>
+
 <?php
 
+error_reporting(0);
 
+if (isset($_POST['submit'])) {
+	include 'proses.php';
+	# code...
+}
 
 ?>
